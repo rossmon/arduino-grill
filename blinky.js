80e194d288e8af812d0e253a8cf452299d34ad2f
Ross Montague <ross@montagues.us>
2015-06-16 20:25:55 -0500

    Added arduino files for grill controller

---
+++ blinky.js
@@ -0,0 +1,17 @@
+var j5 = require("johnny-five");
+var board = new j5.Board();
+ 
+var LEDPIN = 13;
+var OUTPUT = 1;
+ 
+board.on("ready", function(){
+  var val = 0;
+ 
+  // Set pin 13 to OUTPUT mode
+  this.pinMode(LEDPIN, OUTPUT);
+ 
+  // Create a loop to "flash/blink/strobe" an led
+  this.loop( 100, function() {
+    this.digitalWrite(LEDPIN, (val = val ? 0 : 1));
+  });
+});
